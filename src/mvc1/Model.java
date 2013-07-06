f8836a9f0ae3553dafcaabc0bdbd5e92d2cbf05b
Ellen <Ellen@To2By2>
2013-07-06 17:40:34 +0200

    model soort vn

--- src/mvc1/Model.java
+++ src/mvc1/Model.java
@@ -1,10 +1,19 @@
 package mvc1;
 
+import java.util.List;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
-import util.Building;
-import util.Item;
-import util.Person;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import lezen.Building;
+import lezen.Item;
+import lezen.Person;
+import lezen.World;
+import xml2beeld.vijf;
 
 /**
  *
@@ -13,14 +22,74 @@ import util.Person;
 public class Model {
     private double x;
     private double y;
-    private Shape me;
+    private static final Shape me = new Rectangle(5.0, 10.0);;
     
-    private ObservableList<Building> buildings;
-    private ObservableList<Person> persons;
-    private ObservableList<Item> items;
+    /*
+     * illegaal
+     * beste manier om verandering weer te geven??
+     */
+    private List children;
     
-    //voegtoe en verwijder
+    private final ObservableList<Building> buildings = FXCollections.observableArrayList();
+    private final ObservableList<Person> persons = FXCollections.observableArrayList();
+    private final ObservableList<Item> items = FXCollections.observableArrayList();
+
+    public Model(Group root) {
+            me.setFill(Color.YELLOW);
+            x=20; y=20;
+
+            this.children=root.getChildren();
+            
+        World w;
+        try {
+            JAXBContext jc = JAXBContext.newInstance(World.class);
+            w = (World) jc.createUnmarshaller().unmarshal(vijf.class.getResource("Wereld.xml"));
+        } catch (JAXBException ex) {
+            throw new RuntimeException("JAXB:" + ex);
+        } 
+        
+        for(Building b : w.getBuildings()){
+           buildings.add(b);
+           children.add(b.getShape());
+        }
+        
+        for(Person b : w.getPersons()){
+            persons.add(b);
+            children.add(b.getShape());
+        }
+        
+        for(Item b : w.getItems()){
+            items.add(b);
+            children.add(b.getShape());
+        }
+
+        children.add(me);
+
+    }
+
+    public ObservableList<Building> getBuildings() {
+        return buildings;
+    }
+
+    public ObservableList<Person> getPersons() {
+        return persons;
+    }
+
+    public ObservableList<Item> getItems() {
+        return items;
+    }
     
-    //verplaats mij
+    public void verwijder(Item item){
+        children.remove(item.getShape());
+        items.remove(item);
+    }
+    
+    public void verplaats(double x, double y){
+        me.setTranslateX(x);
+        me.setTranslateY(y);
+    }
+    
+
+
     
 }
