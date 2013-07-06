4b8f89d44f0bbaf055332160691aa2e39f9171a7
Ellen <Ellen@To2By2>
2013-07-06 15:47:22 +0200

    xml: - building-item-person (figureke)

--- src/lezen/Building.java
+++ src/lezen/Building.java
@@ -1,12 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package lezen;
 
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
-import javax.xml.bind.annotation.XmlAttribute;
 
 /**
  *
